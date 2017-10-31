8be227fa065059ddb92faf61104d284d55e7e509
Payton Swick <payton@foolord.com>
2017-10-31 17:54:51 -0400

    Apply lint changes to JustSnapsTest

--- tests/JustSnapsTest.php
+++ tests/JustSnapsTest.php
@@ -1,16 +1,18 @@
 <?php
 
+namespace JustSnaps;
+
 class JustSnapsTest extends \PHPUnit\Framework\TestCase {
-	public function test_works() {
+	public function testWorks() {
 		$data = [
 			'a' => 'b',
 		];
-		$snap_file_data = json_encode( $data );
-		$snap_file_driver = JustSnaps\FileDriver::buildWithData( [
+		$snap_file_data = json_encode($data);
+		$snap_file_driver = FileDriver::buildWithData([
 			'foobar' => $snap_file_data,
-		] );
-		$matcher = new JustSnaps\Matcher();
-		$asserter = new JustSnaps\Asserter( $snap_file_driver, $matcher );
-		$this->assertTrue( $asserter->forTest( 'foobar' )->assertMatchesSnapshot( $data ) );
+		]);
+		$matcher = new Matcher();
+		$asserter = new Asserter($snap_file_driver, $matcher);
+		$this->assertTrue($asserter->forTest('foobar')->assertMatchesSnapshot($data));
 	}
 }

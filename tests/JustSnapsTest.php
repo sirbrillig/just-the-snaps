821c9bbdf074727d879f39d0eb2263ab36d3ccfc
Payton Swick <payton@foolord.com>
2017-11-01 16:39:14 -0400

    Add tests for failing snapshot comparisons

--- tests/JustSnapsTest.php
+++ tests/JustSnapsTest.php
@@ -32,10 +32,60 @@ class JustSnapsTest extends \PHPUnit\Framework\TestCase {
 		$this->assertTrue($asserter->forTest('foobar')->assertMatchesSnapshot($data));
 	}
 
+	public function testAssertMatchesSnapshotFailsIfDataChanges() {
+		$data = [
+			'a' => 'b',
+		];
+		$snap_file_driver = FileDriver::buildWithData([]);
+		$matcher = new Matcher();
+		$asserter = new Asserter($snap_file_driver, $matcher);
+		try {
+			$asserter->forTest('foobar')->assertMatchesSnapshot($data);
+		} catch (CreatedSnapshotException $err) {
+			$err; // noop
+		}
+		$data['a'] = 'x';
+		$this->assertFalse($asserter->forTest('foobar')->assertMatchesSnapshot($data));
+	}
+
+	public function testAssertMatchesSnapshotFailsIfDataIsAdded() {
+		$data = [
+			'a' => 'b',
+		];
+		$snap_file_driver = FileDriver::buildWithData([]);
+		$matcher = new Matcher();
+		$asserter = new Asserter($snap_file_driver, $matcher);
+		try {
+			$asserter->forTest('foobar')->assertMatchesSnapshot($data);
+		} catch (CreatedSnapshotException $err) {
+			$err; // noop
+		}
+		$data['b'] = 'x';
+		$this->assertFalse($asserter->forTest('foobar')->assertMatchesSnapshot($data));
+	}
+
+	public function testAssertMatchesSnapshotFailsIfDataIsRemoved() {
+		$data = [
+			'a' => 'b',
+			'c' => 'd',
+		];
+		$snap_file_driver = FileDriver::buildWithData([]);
+		$matcher = new Matcher();
+		$asserter = new Asserter($snap_file_driver, $matcher);
+		try {
+			$asserter->forTest('foobar')->assertMatchesSnapshot($data);
+		} catch (CreatedSnapshotException $err) {
+			$err; // noop
+		}
+		$dataChanged = [ 'a' => 'b' ];
+		$this->assertFalse($asserter->forTest('foobar')->assertMatchesSnapshot($dataChanged));
+	}
+
 	public function testFileDriverDirectory() {
 		$data = [
 			'a' => 'b',
 		];
+		// TODO: delete all snapshots first
 		$snap_file_driver = FileDriver::buildWithDirectory('./tests/__snapshots__');
 		$matcher = new Matcher();
 		$asserter = new Asserter($snap_file_driver, $matcher);
@@ -45,5 +95,6 @@ class JustSnapsTest extends \PHPUnit\Framework\TestCase {
 			$err; // noop
 		}
 		$this->assertTrue($asserter->forTest('foobar')->assertMatchesSnapshot($data));
+		// TODO: delete all snapshots after
 	}
 }

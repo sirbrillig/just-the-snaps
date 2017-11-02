c0a3cb815efb04dcf5fa767a183a3f98e01ae89a
Payton Swick <payton@foolord.com>
2017-11-02 13:41:29 -0400

    Clean up CreatedSnapshotException using class_alias

--- src/JustSnaps/CreatedSnapshotException.php
+++ src/JustSnaps/CreatedSnapshotException.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 namespace JustSnaps;
 
 if (class_exists('\PHPUnit_Framework_IncompleteTestError')) {
-	class CreatedSnapshotException extends \PHPUnit_Framework_IncompleteTestError {
-	}
-}
-else if (class_exists('\PHPUnit\Framework\IncompleteTestError')) {
-	class CreatedSnapshotException extends \PHPUnit\Framework\IncompleteTestError {
-	}
+	class_alias(\PHPUnit_Framework_IncompleteTestError::class, 'JustSnaps\IncompleteTestError');
+} elseif (class_exists('\PHPUnit\Framework\IncompleteTestError')) {
+	class_alias(\PHPUnit\Framework\IncompleteTestError::class, 'JustSnaps\IncompleteTestError');
 } else {
-	class CreatedSnapshotException extends \Exception {
-	}
+	class_alias(\Exception::class, 'JustSnaps\IncompleteTestError');
+}
+
+class CreatedSnapshotException extends IncompleteTestError {
 }
